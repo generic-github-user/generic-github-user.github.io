ccf72803c4d2672ca382fd3baf3c58e3d1ee6bcf
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-04-30 16:35:33 -0400

    Add text

--- site/main.js
+++ site/main.js
@@ -1,3 +1,30 @@
+
+// Array shuffling function from https://stackoverflow.com/a/12646864
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+function animate(e, delay) {
+      let content = e.text();
+      function step(c, i) {
+            e.text(c);
+            c += content[i];
+            if (i < content.length) {
+                  setTimeout(() => step(c, i+1), delay);
+            }
+      }
+      step("", 0);
+}
+
 $(function(){
-      $("#header").load("./header.html");
+      $("#header").load("./header.html", function() {
+            $(".animate").each(function () {
+                  animate($(this), 100);
+            });
+      });
 });
